272233021e6849c94d1caa30c1da5c0bbcaeaf59
Gabrielende5 <gabrielende53@gmail.com>
2023-05-18 16:34:10 -0300

    pegando os dados

---
+++ scripts/FetchCartoons.js
@@ -0,0 +1,5 @@
+var urlCartoon = `https://api.sampleapis.com/cartoons/cartoons2D`
+fetch(urlCartoon)
+    .then(res=>res.json())
+    .then(data=>console.log(data))
+    .catch(erro =>console.log(erro))
---
+++ scripts/navbar.js
@@ -0,0 +1,7 @@
+const navbar = document.getElementById("navbar")
+navbar.innerHTM = `
+    <div>
+    <a class="links-navbar" href="./index.html">Home</a>
+    <a class="links-navbar" href="./cartoons.html">Cartoons</a>
+    </div>
+    <img src="./imagens/navbar/1.jpeg" id="navbar-image"> `
