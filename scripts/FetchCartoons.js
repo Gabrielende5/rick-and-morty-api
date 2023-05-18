03ff6ae3963a20b524de15314a9341f3d28d5fa1
Gabrielende5 <gabrielende53@gmail.com>
2023-05-18 17:31:13 -0300

    imagens

--- scripts/FetchCartoons.js
+++ scripts/FetchCartoons.js
@@ -1,5 +1,17 @@
 var urlCartoon = `https://api.sampleapis.com/cartoons/cartoons2D`
 fetch(urlCartoon)
     .then(res=>res.json())
-    .then(data=>console.log(data))
+    .then(data=>PopularATela(data))
     .catch(erro =>console.log(erro))
+
+function PopularATela(data){
+    const div = document.getElementById('cartoonsList');
+    const allCards = data.map(item=>`
+        <div>
+            <h2>${item.title}<h2>
+            <img class = "imageCard" src="${item.image}" alt = "imagem não achada ${item.title}"/>
+        </div>
+    `).join('');
+    div.innerHTML = allCards;
+
+}
