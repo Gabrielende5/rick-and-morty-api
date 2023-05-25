8aee2cc9980bf9c8162c9185513562ac29111f1d
Gabrielende5 <gabrielende53@gmail.com>
2023-05-25 16:34:28 -0300

    nome

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,7 +1,23 @@
 const navbar = document.getElementById("navbar")
-navbar.innerHTM = `
+const checkNavName = localStorage.getItem('nome')
+navbar.innerHTML = `
     <div>
     <a class="links-navbar" href="./index.html">Home</a>
     <a class="links-navbar" href="./cartoons.html">Cartoons</a>
     </div>
-    <img src="./imagens/navbar/1.jpeg" id="navbar-image"> `
+    <div id="info-navbar">
+        <div id = "nome-navbar"><p id ="nome-na-navbar">${checkNavName}</p></div>
+        <img onclick ="logar()" src="./imagens/navbar/1.jpeg" id="navbar-image"> 
+    </div>
+`
+function logar(){
+    var userName = prompt('Por favor coloque seu nome: ')
+    localStorage.setItem('nome', userName)
+    const userNameCampo = document.getElementById('nome-navbar')
+    userNameCampo.innerHTML = `<p id ="nome-na-navbar"> ${userName}</p>`
+    if(userName === null)
+    {
+        userNameCampo.innerHTML = ` <p id = "nome-navbar"> </p>`
+    }
+}
+
